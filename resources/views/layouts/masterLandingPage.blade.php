80d16828aa56066ccbc8c8640f44801bab7b652e
cantsleepbeforecode <samringo25@gmail.com>
2020-04-26 22:02:27 +0800

    final touch before hosting

--- resources/views/layouts/masterLandingPage.blade.php
+++ resources/views/layouts/masterLandingPage.blade.php
@@ -131,15 +131,6 @@
                     <i class="fas fa-bars"></i>
                   </button>
                 </div>
-                <div class="order-1 order-lg-2">
-                  <div class="d-inline-flex">
-                    <ul class="header-social-icons social-icons custom-social-icons-style-1 _white d-none d-sm-block">
-                      <li class="social-icons-facebook"><a href="http://www.facebook.com/" target="_blank" title="Facebook"><i class="fab fa-facebook-square"></i></a></li>
-                      <li class="social-icons-twitter"><a href="http://www.twitter.com/" target="_blank" title="Twitter"><i class="fab fa-twitter-square"></i></a></li>
-                      <li class="social-icons-linkedin"><a href="http://www.linkedin.com/" target="_blank" title="Linkedin"><i class="fab fa-linkedin"></i></a></li>
-                    </ul>
-                  </div>
-                </div>
               </div>
             </div>
           </div>
